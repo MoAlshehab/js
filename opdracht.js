16dc3628fedd895cd838b1c2111615e21f78f910
Moal <97049235@st.deltion.nl>
2022-01-04 22:42:58 +0100

    het is voor js

---
+++ opdracht.js
@@ -0,0 +1,9 @@
+alert("Hello world")
+document.write ("Deltion college")
+confirm("het is oke we gaan het zien ")
+prompt("Wat is uw leeftijd? ")
+a = 5;
+b = 6;
+c = 10;
+d = a + c + b;
+console.log(d);
