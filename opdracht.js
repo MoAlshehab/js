767f7773794421d89f37a4d217b54e6741c03dc2
Moal <97049235@st.deltion.nl>
2022-01-12 20:52:07 +0100

    het is nogen keer gedaan

--- opdracht.js
+++ opdracht.js
@@ -2,8 +2,30 @@ alert("Hello world")
 document.write ("Deltion college")
 confirm("het is oke we gaan het zien ")
 prompt("Wat is uw leeftijd? ")
-a = 5;
-b = 6;
-c = 10;
-d = a + c + b;
-console.log(d);
+
+// vereabels
+var a = 5;
+var b = 6;
+var c = 10;
+let = a + b * c;
+console.log(let);
+
+/*Funces
+function mo(name, lastName){
+ console.log('hallo'+' ' + name + ' ' + lastName)
+}
+mo('Mo','Alshehab')
+mo('Ali','')*/
+
+function buttonClick(){
+    alert("Er is op de knop geklikt....");
+}
+/*
+function berekenBtw(bedragExBtw){
+    console.log(bedragExBtw *0.21)
+}
+berekenBtw(150)*/
+function berekenBtw(bedragExBtw){
+    return bedragExBtw  * 0.21;
+}
+ berekenBtw(100);
